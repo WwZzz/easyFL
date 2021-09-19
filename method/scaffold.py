adb89d139a05b917ce8a552f062a513fea3928dd
WwZzz <zwang@stu.xmu.edu.cn>
2021-09-19 17:16:28 +0800

    Update scaffold.py

--- method/scaffold.py
+++ method/scaffold.py
@@ -46,7 +46,8 @@ class Server(BaseServer):
 class Client(BaseClient):
     def __init__(self, option, name='', data_train_dict={'x': [], 'y': []}, data_val_dict={'x': [], 'y': []}, train_rate=0.8, drop_rate=0):
         super(Client, self).__init__(option, name, data_train_dict, data_val_dict, train_rate, drop_rate)
-
+        self.c = None
+        
     def train(self, model, cg):
         if not self.c:
             self.c = model.zeros_like()
