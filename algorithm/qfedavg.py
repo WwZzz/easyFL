ef1d62e20784204fdf6cc040a9726cb67122c3ca
WwZzz <zwang@stu.xmu.edu.cn>
2021-12-28 16:31:56 +0800

    easyFL v3.1.0

--- algorithm/qfedavg.py
+++ algorithm/qfedavg.py
@@ -1,12 +1,11 @@
 from .fedbase import BasicServer, BasicClient
 import numpy as np
+from utils import fmodule
 
 class Server(BasicServer):
     def __init__(self, option, model, clients, test_data = None):
         super(Server, self).__init__(option, model, clients, test_data)
         self.q = option['q']
-        self.learning_rate = option['learning_rate']
-        self.L = 1.0/option['learning_rate']
         self.paras_name = ['q']
 
     def iterate(self, t):
@@ -15,10 +14,10 @@ class Server(BasicServer):
         # training
         models, train_losses = self.communicate(self.selected_clients)
         # plug in the weight updates into the gradient
-        grads = [(self.model- model)/self.learning_rate for model in models]
+        grads = [(self.model- model) / self.lr for model in models]
         Deltas = [gi*np.float_power(li + 1e-10, self.q) for gi,li in zip(grads,train_losses)]
         # estimation of the local Lipchitz constant
-        hs = [self.q * np.float_power(li + 1e-10, (self.q - 1)) * (gi.norm() ** 2) + self.L * np.float_power(li + 1e-10, self.q) for gi,li in zip(grads,train_losses)]
+        hs = [self.q * np.float_power(li + 1e-10, (self.q - 1)) * (gi.norm() ** 2) + 1.0 / self.lr * np.float_power(li + 1e-10, self.q) for gi,li in zip(grads,train_losses)]
         # aggregate
         self.model = self.aggregate(Deltas, hs)
         return
@@ -26,9 +25,9 @@ class Server(BasicServer):
     def aggregate(self, Deltas, hs):
         demominator = np.sum(np.asarray(hs))
         scaled_deltas = [delta/demominator for delta in Deltas]
-        updates = sum(scaled_deltas)
-        w_new = self.model - updates
-        return w_new
+        updates = fmodule._model_sum(scaled_deltas)
+        new_model = self.model - updates
+        return new_model
 
 class Client(BasicClient):
     def __init__(self, option, name='', train_data=None, valid_data=None):
