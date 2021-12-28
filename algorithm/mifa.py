7b7cde4381f4d900977ee28a4583d21b1bdd56a2
WwZzz <zwang@stu.xmu.edu.cn>
2021-12-28 14:28:14 +0800

    fix bugs of mifa

--- algorithm/mifa.py
+++ algorithm/mifa.py
@@ -38,8 +38,8 @@ class Server(BasicServer):
 
 
 class Client(BasicClient):
-    def __init__(self, option, name='', train_data=None, valid_data=None, drop_rate=-1):
-        super(Client, self).__init__(option, name, train_data, valid_data, drop_rate)
+    def __init__(self, option, name='', train_data=None, valid_data=None):
+        super(Client, self).__init__(option, name, train_data, valid_data)
 
 
 
