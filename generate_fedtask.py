9e3f93620def2c5baf388abdb708e1fc8b15d48e
WwZzz <zwang@stu.xmu.edu.cn>
2022-05-04 16:08:19 +0800

    add seed to generate_fedtask.py

--- generate_fedtask.py
+++ generate_fedtask.py
@@ -7,6 +7,7 @@ def read_option():
     parser.add_argument('--dist', help='type of distribution;', type=int, default=0)
     parser.add_argument('--skew', help='the degree of niid;', type=float, default=0)
     parser.add_argument('--num_clients', help='the number of clients;', type=int, default=100)
+    parser.add_argument('--seed', help='random seed;', type=int, default=0)
     try: option = vars(parser.parse_args())
     except IOError as msg: parser.error(str(msg))
     return option
@@ -14,5 +15,5 @@ def read_option():
 if __name__ == '__main__':
     option = read_option()
     TaskGen = getattr(importlib.import_module('.'.join(['benchmark', option['benchmark'], 'core'])), 'TaskGen')
-    generator = TaskGen(dist_id = option['dist'], skewness = option['skew'], num_clients=option['num_clients'])
+    generator = TaskGen(dist_id = option['dist'], skewness = option['skew'], num_clients=option['num_clients'], seed = option['seed'])
     generator.run()
