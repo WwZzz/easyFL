8f4d48e7f9daa14a8656b0c1aa114c935f30f948
zzz <zzz510711928@gmail.com>
2022-07-05 15:40:13 +0800

    change the way to specify the algorithm-dependent hyper-parameters by inputting commands with --algo_para; adding the function of operations in Server being done on CPU or other flexible devices.

--- utils/fflow.py
+++ utils/fflow.py
@@ -9,7 +9,7 @@ import utils.fmodule
 import ujson
 import time
 import collections
-import utils.network_simulator as ns
+import utils.systemic_simulator as ss
 
 sample_list=['uniform', 'md']
 agg_list=['uniform', 'weighted_scale', 'weighted_com']
@@ -26,12 +26,12 @@ def read_option():
     # methods of server side for sampling and aggregating
     parser.add_argument('--sample', help='methods for sampling clients', type=str, choices=sample_list, default='md')
     parser.add_argument('--aggregate', help='methods for aggregating models', type=str, choices=agg_list, default='uniform')
-    parser.add_argument('--learning_rate_decay', help='learning rate decay for the training process;', type=float, default=0.998)
-    parser.add_argument('--weight_decay', help='weight decay for the training process', type=float, default=0)
-    parser.add_argument('--lr_scheduler', help='type of the global learning rate scheduler', type=int, default=-1)
     # hyper-parameters of training in server side
     parser.add_argument('--num_rounds', help='number of communication rounds', type=int, default=20)
     parser.add_argument('--proportion', help='proportion of clients sampled per round', type=float, default=0.2)
+    parser.add_argument('--learning_rate_decay', help='learning rate decay for the training process;', type=float, default=0.998)
+    parser.add_argument('--weight_decay', help='weight decay for the training process', type=float, default=0)
+    parser.add_argument('--lr_scheduler', help='type of the global learning rate scheduler', type=int, default=-1)
     # hyper-parameters of local training
     parser.add_argument('--num_epochs', help='number of epochs when clients trainset on data;', type=int, default=5)
     parser.add_argument('--num_steps', help='the number of local steps, which dominate num_epochs when setting num_steps>0', type=int, default=-1)
@@ -39,36 +39,21 @@ def read_option():
     parser.add_argument('--batch_size', help='batch size when clients trainset on data;', type=float, default='64')
     parser.add_argument('--optimizer', help='select the optimizer for gd', type=str, choices=optimizer_list, default='SGD')
     parser.add_argument('--momentum', help='momentum of local update', type=float, default=0)
-
-    # machine environment settings
+    # realistic machine config
     parser.add_argument('--seed', help='seed for random initialization;', type=int, default=0)
-    parser.add_argument('--gpu', help='GPU ID, -1 for CPU', type=int, default=-1)
+    parser.add_argument('--gpu', nargs='*', help='GPU IDs and empty input is equal to using CPU', type=int)
+    parser.add_argument('--server_with_cpu', help='seed for random initialization;', action="store_true", default=False)
     parser.add_argument('--eval_interval', help='evaluate every __ rounds;', type=int, default=1)
     parser.add_argument('--num_threads', help="the number of threads in the clients computing session", type=int, default=1)
     parser.add_argument('--num_workers', help='the number of workers of DataLoader', type=int, default=0)
     parser.add_argument('--test_batch_size', help='the batch_size used in testing phase;', type=int, default=512)
+    parser.add_argument('--project', help='name of this project for wandb;', type=str, default='unknown_project')
+    # the simulating systemic configuration of clients that helps constructing the heterogeity in the network condition & computing power
+    parser.add_argument('--network_config', help="configuration of the availability of clients", type=str, default = 'ideal')
+    parser.add_argument('--computing_config', help="configuration of the computing power of clients", type=str, default = 'ideal')
+    # algorithm-dependent hyper-parameters
+    parser.add_argument('--algo_para', help='algorithm-dependent hyper-parameters', nargs='*', type=float)
 
-    # the simulating system settings of clients
-    # constructing the heterogeity of the network
-    parser.add_argument('--net_drop', help="controlling the dropout of clients after being selected in each communication round according to distribution Beta(drop,1)", type=float, default=0)
-    parser.add_argument('--net_active', help="controlling the probability of clients being active and obey distribution Beta(active,1)", type=float, default=99999)
-    parser.add_argument('--net_latency', help="controlling the variance of network conditions for different clients. The larger it is, the more differences of the network latency there are.", type=float, default=0)
-    # constructing the heterogeity of computing capability
-    parser.add_argument('--capability', help="controlling the difference of local computing capability of each client", type=float, default=0)
-
-    # hyper-parameters of different algorithms
-    parser.add_argument('--learning_rate_lambda', help='η for λ in afl', type=float, default=0)
-    parser.add_argument('--q', help='q in q-fedavg', type=float, default='0.0')
-    parser.add_argument('--epsilon', help='ε in fedmgda+', type=float, default='0.0')
-    parser.add_argument('--eta', help='global learning rate in fedmgda+', type=float, default='1.0')
-    parser.add_argument('--tau', help='the length of recent history gradients to be contained in FedFAvg', type=int, default=0)
-    parser.add_argument('--alpha', help='proportion of clients keeping original direction in FedFV/alpha in fedFA', type=float, default='0.0')
-    parser.add_argument('--beta', help='beta in FedFA',type=float, default='1.0')
-    parser.add_argument('--gamma', help='gamma in FedFA', type=float, default='0')
-    parser.add_argument('--mu', help='mu in fedprox', type=float, default='0.1')
-    parser.add_argument('--alg', help='clustered sampling', type=int, default=1)
-    parser.add_argument('--w', help='whether to wait for all updates being initialized before aggregation', type=int, default=1)
-    parser.add_argument('--c', help='proportion of clients keeping original direction in FedFV/alpha in fedFA', type=float, default='0.0')
     try: option = vars(parser.parse_args())
     except IOError as msg: parser.error(str(msg))
     return option
@@ -87,16 +72,21 @@ def initialize(option):
     bmk_name = option['task'][:option['task'].find('cnum')-1].lower()
     bmk_model_path = '.'.join(['benchmark', bmk_name, 'model', option['model']])
     bmk_core_path = '.'.join(['benchmark', bmk_name, 'core'])
-    utils.fmodule.device = torch.device('cuda:{}'.format(option['gpu']) if torch.cuda.is_available() and option['gpu'] != -1 else 'cpu')
+    # init gpu
+    gpus = option['gpu']
+    utils.fmodule.dev_list = [torch.device('cpu')] if len(gpus)==0 else [torch.device('cuda:{}'.format(gpu_id)) for gpu_id in gpus]
+    utils.fmodule.dev_manager = utils.fmodule.get_device()
     utils.fmodule.TaskCalculator = getattr(importlib.import_module(bmk_core_path), 'TaskCalculator')
-    utils.fmodule.TaskCalculator.setOP(getattr(importlib.import_module('torch.optim'), option['optimizer']))
     # The Model is defined in bmk_model_path as default, whose filename is option['model'] and the classname is 'Model'
     # If an algorithm change the backbone for a task, a modified model should be defined in the path 'algorithm/method_name.py', whose classname is option['model']
     try:
         utils.fmodule.Model = getattr(importlib.import_module(bmk_model_path), 'Model')
     except ModuleNotFoundError:
         utils.fmodule.Model = getattr(importlib.import_module('.'.join(['algorithm', option['algorithm']])), option['model'])
-    model = utils.fmodule.Model().to(utils.fmodule.device)
+    if not option['server_with_cpu']:
+        model = utils.fmodule.Model().to(utils.fmodule.dev_list[0])
+    else:
+        model = utils.fmodule.Model()
     # init the model that owned by the server (e.g. the model trained in the server-side)
     try:
         utils.fmodule.SvrModel = getattr(importlib.import_module(bmk_model_path), 'SvrModel')
@@ -124,15 +114,15 @@ def initialize(option):
     print('init clients...', end='')
     client_path = '%s.%s' % ('algorithm', option['algorithm'])
     Client=getattr(importlib.import_module(client_path), 'Client')
-    clients = [Client(option, name = client_names[cid], train_data = train_datas[cid], valid_data = valid_datas[cid]) for cid in range(num_clients)]
+    clients = [Client(option, name=client_names[cid], train_data=train_datas[cid], valid_data=valid_datas[cid]) for cid in range(num_clients)]
     print('done')
 
     # init server
     print("init server...", end='')
     server_path = '%s.%s' % ('algorithm', option['algorithm'])
     server = getattr(importlib.import_module(server_path), 'Server')(option, model, clients, test_data = test_data)
-    # init virtual network environment
-    ns.init_network_environment(server)
+    # init virtual systemic configuration including network state and the distribution of computing power
+    ss.init_systemic_config(server, option)
     # init logger
     try:
         Logger = getattr(importlib.import_module(server_path), 'MyLogger')
@@ -145,20 +135,18 @@ def initialize(option):
 
 def output_filename(option, server):
     header = "{}_".format(option["algorithm"])
-    for para in server.paras_name: header = header + para + "{}_".format(option[para])
-    output_name = header + "M{}_R{}_B{}_E{}_NS{}_LR{:.4f}_P{:.2f}_S{}_LD{:.3f}_WD{:.3f}_DR{:.2f}_AC{:.2f}_.json".format(
-        option['model'],
-        option['num_rounds'],
-        option['batch_size'],
-        option['num_epochs'],
-        option['num_steps'],
-        option['learning_rate'],
-        option['proportion'],
-        option['seed'],
-        option['lr_scheduler']+option['learning_rate_decay'],
-        option['weight_decay'],
-        option['net_drop'],
-        option['net_active'])
+    for para,pv in server.algo_para.items(): header = header + para + "{}_".format(pv)
+    output_name = header + "M{}_R{}_B{}_".format(option['model'], option['num_rounds'],option['batch_size'])+ \
+                  ("E{}".format(option['num_epochs']) if option['num_steps']<0 else "K{}".format(option['num_steps']))+\
+                  "LR{:.4f}_P{:.2f}_S{}_LD{:.3f}_WD{:.3f}_NET{}_CMP{}_.json".format(
+                      option['learning_rate'],
+                      option['proportion'],
+                      option['seed'],
+                      option['lr_scheduler']+option['learning_rate_decay'],
+                      option['weight_decay'],
+                      option['network_config'],
+                      option['computing_config']
+                  )
     return output_name
 
 class Logger:
@@ -214,13 +202,13 @@ class DefaultLogger(Logger):
         for met_name, met_val in test_metric.items():
             self.output['test_' + met_name].append(met_val)
         # calculate weighted averaging of metrics of training datasets across clients
-        # train_metrics = server.test_on_clients(self.current_round, 'train')
-        # for met_name, met_val in train_metrics.items():
-        #     self.output['train_' + met_name].append(1.0 * sum([client_vol * client_met for client_vol, client_met in zip(server.client_vols, met_val)]) / server.data_vol)
+        train_metrics = server.test_on_clients(self.current_round, 'train')
+        for met_name, met_val in train_metrics.items():
+            self.output['train_' + met_name].append(1.0 * sum([client_vol * client_met for client_vol, client_met in zip(server.local_data_vols, met_val)]) / server.total_data_vol)
         # calculate weighted averaging and other statistics of metrics of validation datasets across clients
         valid_metrics = server.test_on_clients(self.current_round, 'valid')
         for met_name, met_val in valid_metrics.items():
-            self.output['valid_' + met_name].append(1.0 * sum([client_vol * client_met for client_vol, client_met in zip(server.client_vols, met_val)]) / server.data_vol)
+            self.output['valid_' + met_name].append(1.0 * sum([client_vol * client_met for client_vol, client_met in zip(server.local_data_vols, met_val)]) / server.total_data_vol)
             self.output['mean_valid_' + met_name].append(np.mean(met_val))
             self.output['std_valid_' + met_name].append(np.std(met_val))
         # output to stdout
