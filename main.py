64935a04b563f5b43011cbeb85dbdc0ae6338535
zzz <zzz510711928@gmail.com>
2022-07-05 15:43:23 +0800

    additionally initialize torch.multiprocessing

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import utils.fflow as flw
+import torch
 
 def main():
     # read options
     option = flw.read_option()
     # set random seed
     flw.setup_seed(option['seed'])
-    # initialize server
+    # initialize server, clients and fedtask
     server = flw.initialize(option)
     # start federated optimization
     server.run()
 
 if __name__ == '__main__':
+    torch.multiprocessing.set_start_method('spawn')
+    torch.multiprocessing.set_sharing_strategy('file_system')
     main()
