d0f70325c7b1746a0ed8f55d56437e86b9af4848
zzz <zzz510711928@gmail.com>
2022-07-27 17:34:36 +0800

    tiny changes on simple_logger

--- utils/logger/simple_logger.py
+++ utils/logger/simple_logger.py
@@ -2,6 +2,11 @@ import utils.logger.basic_logger as bl
 import numpy as np
 
 class Logger(bl.Logger):
+    def initialize(self):
+        """This method is used to record the stastic variables that won't change across rounds (e.g. local data size)"""
+        for c in self.clients:
+            self.output['client_datavol'].append(len(c.train_data))
+
     """This logger only records metrics on validation dataset"""
     def log_per_round(self, *args, **kwargs):
         valid_metrics = self.server.test_on_clients('valid')
