041ded421de17415a397395a7df45bdd8f28fed6
zzz <zzz510711928@gmail.com>
2022-08-02 13:49:19 +0800

    add virtual clock for the FL system

--- utils/systemic_simulator.py
+++ utils/systemic_simulator.py
@@ -4,9 +4,61 @@ Here we implement three different network heterogeneity for a FL system:
     1. with_accessibility: some clients are not available during the stage of sampling
     2. with_latency: accumulating latencies of clients and dropout the overdue clients
 """
+import collections
+
 import numpy as np
-TIME_UNIT = 1
+
+class VirtualClock:
+    _TIME_UNIT = 1
+    _MAX_TIME_WAIT = 1
+    _TIME_STAMP = []
+    def __init__(self):
+        self.active_stamps = []
+        self.client_time_response = []
+        self.client_time_active = []
+        self.cost_per_round = []
+
+    def add_active_stamp(self, active_stamp):
+        """
+        Record the round-wise clients active stamps
+        :param active_stamp: a list of lists of active clients at each time unit
+        """
+        self.active_stamps.append(active_stamp)
+        client_time_active = {cid:(t+1)*self._TIME_UNIT for t in range(len(active_stamp)) for cid in active_stamp[t]}
+        self.client_time_active.append(client_time_active)
+
+    def add_response_time(self, client_time_response):
+        """
+        Record the round-wise clients' response intervals
+        :param client_time_response: a dict where (key, value) = (client_id, current_round_response_interval)
+        """
+        self.client_time_response.append(client_time_response)
+
+    def communication_timer(self, due_response):
+        client_reach_time = {cid: self.client_time_active[-1][cid] + self.client_time_response[-1][cid] for cid in self.client_time_response[-1].keys()}
+        # filter clients within the due
+        effective_clients = []
+        while len(effective_clients)==0:
+            effective_clients = [cid for cid in client_reach_time.keys() if client_reach_time[cid]<=(len(self.active_stamps[-1])+due_response)]
+            due_response += 1
+        if len(effective_clients)==len(client_reach_time):
+            cost = max(max([time_reached for time_reached in client_reach_time.values()]), len(self.active_stamps[-1]))
+        else:
+            cost = len(self.active_stamps[-1]) + due_response
+        self.cost_per_round.append(cost*self._TIME_UNIT)
+        return effective_clients
+
+    @property
+    def latest_active_stamp(self):
+        if len(self.active_stamps)>0: return self.active_stamps[-1]
+        else: return []
+
+    @property
+    def timestamp(self):
+        return np.cumsum(self.cost_per_round).tolist()
+
 random_seed_gen = None
+clock = VirtualClock()
 
 def seed_generator(seed=0):
     while True:
@@ -16,16 +68,22 @@ def seed_generator(seed=0):
 def update_activity(server, random_module=np.random):
     return
 
+def update_dropout(server, clients=[], random_module=np.random):
+    return
+
 def update_local_computing_resource(server, clients=[], random_module=np.random):
+    return
+
+def update_response_time(server, clients=[], random_module=np.random):
     return
 
 def init_network_mode(server, mode='ideal'):
     global update_activity
     if mode=='ideal':
         for c in server.clients:
-            c.network_active_rate = 1
-            c.network_drop_rate = 0
-            c.network_latency_amount = 0
+            c.network_active_rate = 0.5
+            c.network_drop_rate = 0.6
+            c.time_response = 5
 
     elif mode.startswith('MIFA'):
         """
@@ -43,7 +101,7 @@ def init_network_mode(server, mode='ideal'):
         label_num = len(label_counter(server.test_data))
         for c in server.clients:
             c.network_drop_rate = 0
-            c.network_latency_amount = 0
+            c.time_response = 0
             c_counter = label_counter(c.train_data+c.valid_data)
             c_label = [lb for lb in c_counter.keys()]
             c.network_active_rate = (pmin * min(c_label) / max(1, label_num-1)) + (1 - pmin)
@@ -56,7 +114,7 @@ def init_network_mode(server, mode='ideal'):
         for c in server.clients:
             c.network_active_rate = 0.8
             c.network_drop_rate = 0
-            c.network_latency_amount = 0
+            c.time_response = 0
 
     elif mode=='F3AST-Home':
         Tks = [np.random.lognormal(0,0.5) for _ in server.clients]
@@ -64,7 +122,7 @@ def init_network_mode(server, mode='ideal'):
         for c,Tk in zip(server.clients, Tks):
             c.network_active_rate = 1.0 * Tk / max_Tk
             c.network_drop_rate = 0
-            c.network_latency_amount = 0
+            c.time_response = 0
 
     elif mode=='F3AST-Smartphones':
         def f(server, random_module=np.random):
@@ -80,14 +138,14 @@ def init_network_mode(server, mode='ideal'):
             c.qk = 1.0 * Tk / max_Tk
             c.network_active_rate = 1
             c.network_drop_rate = 0
-            c.network_latency_amount = 0
+            c.time_response = 0
 
     elif mode=='F3AST-Uneven':
         max_data_vol = max(server.local_data_vols)
         for c, cvol in zip(server.clients, server.local_data_vols):
             c.network_active_rate = 1.0 * cvol / max_data_vol
             c.network_drop_rate = 0
-            c.network_latency_amount = 0
+            c.time_response = 0
 
     elif mode.startswith('SmallFirst'):
         k = float(mode[mode.find('k') + 1:]) if mode.find('k') != -1 else 0
@@ -98,13 +156,13 @@ def init_network_mode(server, mode='ideal'):
         for c, pc in zip(server.clients, prop):
             c.network_active_rate = pc
             c.network_drop_rate = 0
-            c.network_latency_amount = 0
+            c.time_response = 0
 
     else:
         for c in server.clients:
             c.network_active_rate = 1
             c.network_drop_rate = 0
-            c.network_latency_amount = 0
+            c.time_response = 0
         return
 
 def init_computing_mode(server, mode='ideal'):
@@ -168,32 +226,59 @@ def init_systemic_config(server, option):
 
 # sampling phase
 def with_inactivity(sample):
-    def sample_with_active(self, all_clients=None):
+    def sample_with_active(self):
         global random_seed_gen
         random_module = np.random.RandomState(next(random_seed_gen))
         update_activity(self, random_module)
         active_clients = []
         while len(active_clients)==0:
             active_clients = [cid for cid in range(self.num_clients) if self.clients[cid].is_active(random_module)]
-        selected_clients = sample(self, all_clients=active_clients)
+        selected_clients = sample(self)
+        effective_clients = set(selected_clients).intersection(set(active_clients))
+        active_stamp = [list(effective_clients)]
+        # wait until the time expired or all the clients are active
+        t = 1
+        while t<self.due_active and len(effective_clients)<len(set(selected_clients)):
+            active_clients = [cid for cid in range(self.num_clients) if self.clients[cid].is_active(random_module)]
+            client_to_be_active = set(selected_clients).difference(effective_clients)
+            new_active_clients = list(client_to_be_active.intersection(active_clients))
+            effective_clients = effective_clients.union(new_active_clients)
+            active_stamp.append(new_active_clients)
+            t += 1
+        clock.add_active_stamp(active_stamp)
+        selected_clients = [cid for cid in selected_clients if cid in effective_clients]
         return selected_clients
     return sample_with_active
 
 # communication phase
 def with_dropout(communicate):
     def communicate_with_dropout(self, selected_clients):
+        global random_seed_gen
+        random_module = np.random.RandomState(next(random_seed_gen))
         for c in self.clients: c.dropped = False
-        self.selected_clients = [selected_clients[i] for i in range(len(selected_clients)) if not self.clients[selected_clients[i]].is_drop()]
+        update_dropout(self, selected_clients, random_module)
+        # drop out clients but with at least one client replying
+        while True:
+            self.selected_clients = [selected_clients[i] for i in range(len(selected_clients)) if not self.clients[selected_clients[i]].is_drop(random_module)]
+            if len(self.selected_clients)>0: break
         return communicate(self, self.selected_clients)
     return communicate_with_dropout
 
-def with_latency(communicate):
-    def communicate_with_latency(self, selected_clients):
-        client_latencies = [self.clients[cid].get_network_latency() for cid in selected_clients]
-        time_sync = min(max(client_latencies), self.TIME_LATENCY_BOUND)
-        self.virtual_clock['time_sync'].append(time_sync)
-        return communicate(self, selected_clients)
-    return communicate_with_latency
+def with_due(communicate):
+    def communicate_with_due(self, selected_clients):
+        global random_seed_gen
+        random_module = np.random.RandomState(next(random_seed_gen))
+        update_response_time(self, selected_clients, random_module)
+        # calculate time response
+        client_time_response = {cid:self.clients[cid].get_time_response() for cid in selected_clients}
+        # the dropped clients' time-response will be inf
+        active_stamp = clock.latest_active_stamp
+        client_dropped = {cid:np.inf for t in range(len(active_stamp)) for cid in active_stamp[t] if cid not in selected_clients}
+        client_time_response.update(client_dropped)
+        clock.add_response_time(client_time_response)
+        self.selected_clients = clock.communication_timer(self.due_response)
+        return communicate(self, self.selected_clients)
+    return communicate_with_due
 
 def with_incomplete_update(communicate):
     def communicate_with_incomplete_update(self, selected_clients):
