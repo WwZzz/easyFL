ab120ec529214b587a30312aba1b9f9ba36ecc74
zwang <zwang@stu.xmu.edu.cn>
2023-02-09 11:25:44 +0800

    update test.py

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
-import flgo
 import flgo.algorithm.fedavg
 import flgo.experiment.analyzer
 import os
-#
+
+# generate federated task if task doesn't exist
 task = './my_task'
 if not os.path.exists(task):
     flgo.gen_task('gen_config.yml', task)
 
-runner = flgo.init(task, {'gpu':[0], 'num_rounds':1, 'log_file':True}, flgo.algorithm.fedavg)
+# running fedavg on the specified task
+runner = flgo.init(task, {'gpu':[0], 'num_rounds':5, 'log_file':True, 'num_steps':5}, flgo.algorithm.fedavg)
 runner.run()
 
+# visualize the experimental result
 flgo.experiment.analyzer.show('res_config.yml')
