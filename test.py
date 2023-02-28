58ffc764bdcc9418c5dae7590c8bd6017697b8a9
zzz <zzz510711928@gmail.com>
2023-02-28 13:33:44 +0800

    modify test.py

--- test.py
+++ test.py
@@ -1,20 +1,22 @@
 import flgo.algorithm.fedavg as fedavg
 import flgo.experiment.analyzer
 import os
-import torch.multiprocessing as mp
 
+task = './my_task'
+gen_config = {
+    'benchmark':{'name':'mnist_classification'},
+    'partitioner':{'name':'IIDPartitioner', 'para':{'num_clients':100}}
+}
+analysis_plan = {
+    'Selector':{'task':'./my_task', 'header':['fedavg',], },
+    'Painter':{'Curve':[{'args':{'x':'communication_round', 'y':'valid_loss'}}]},
+    'Table':{'min_value':[{'x':'valid_loss'}]},
+}
 if __name__ == '__main__':
-    mp.set_start_method('spawn', force=True)
-    mp.set_sharing_strategy('file_system')
-
     # generate federated task if task doesn't exist
-    task = './my_task'
-    if not os.path.exists(task):
-        flgo.gen_task('gen_config.yml', task_path=task)
-
+    if not os.path.exists(task): flgo.gen_task(gen_config, task_path=task)
     # running fedavg on the specified task
     runner = flgo.init(task, fedavg, {'gpu':[0,],'log_file':True, 'num_steps':5})
     runner.run()
-
     # visualize the experimental result
-    flgo.experiment.analyzer.show('res_config.yml')
+    flgo.experiment.analyzer.show(analysis_plan)
