28a584163e188f0e3329bf5adb9b95525ea70094
LafinHana <lafinhana@gmail.com>
2023-04-25 13:06:02 +0800

    Changes to be committed: 	new file:   flgo/benchmark/heart_disease_classification/__init__.py 	new file:   flgo/benchmark/heart_disease_classification/core.py 	new file:   flgo/benchmark/heart_disease_classification/model/__init__.py 	new file:   flgo/benchmark/heart_disease_classification/model/lr.py
    	modified:   flgo/benchmark/toolkits/tabular/classification/__init__.py
    	modified:   flgo/benchmark/toolkits/tabular/classification/datasets.py

---
+++ flgo/benchmark/heart_disease_classification/__init__.py
@@ -0,0 +1,6 @@
+import flgo.benchmark.toolkits.partition
+import flgo.benchmark.heart_disease_classification.model.lr as lr
+
+default_partitioner = flgo.benchmark.toolkits.partition.IIDPartitioner
+default_partition_para = {'num_clients':100}
+default_model = lr
