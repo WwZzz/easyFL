3f0b984383d0790ee4fb50abc552c34b4ae8da1e
zzz <zzz510711928@gmail.com>
2023-07-05 14:55:10 +0800

    add multi-30k

---
+++ flgo/benchmark/multi30k_translation/config.py
@@ -0,0 +1,133 @@
+from torchtext.datasets import Multi30k
+import flgo.benchmark
+import os
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+import random
+
+language_pair = ['de', 'en']
+path = os.path.join(flgo.benchmark.path, 'RAW_DATA', 'MULTI30K')
+train_data, val_data, test_data = Multi30k(split=('train', 'valid', 'test'), language_pair=language_pair)
+
+# init tokenizers
+tokenizers = {}
+tokenizers[language_pair[0]] = get_tokenizer('spacy', language='de_core_news_sm')
+tokenizers[language_pair[1]] = get_tokenizer('spacy', language='en_core_web_sm')
+
+#  init vocabs
+vocabs = {}
+UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
+special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
+for i,ln in enumerate(language_pair):
+    # Create torchtext's Vocab object
+    tokenizer = tokenizers[ln]
+    all_tokens = [tokenizer(d[i]) for d in train_data]
+    vocabs[ln] = build_vocab_from_iterator(all_tokens,
+                                           min_freq=1,
+                                           specials=special_symbols,
+                                           special_first=True)
+for ln in language_pair: vocabs[ln].set_default_index(UNK_IDX)
+
+def init_weights(m):
+    for name, param in m.named_parameters():
+        if 'weight' in name:
+            nn.init.normal_(param.data, mean=0, std=0.01)
+        else:
+            nn.init.constant_(param.data, 0)
+
+def get_model():
+    INPUT_DIM = len(vocabs[language_pair[0]])
+    OUTPUT_DIM = len(vocabs[language_pair[1]])
+    ENC_EMB_DIM = 256
+    DEC_EMB_DIM = 256
+    ENC_HID_DIM = 512
+    DEC_HID_DIM = 512
+    ENC_DROPOUT = 0.5
+    DEC_DROPOUT = 0.5
+    attn = Attention(ENC_HID_DIM, DEC_HID_DIM)
+    enc = Encoder(INPUT_DIM, ENC_EMB_DIM, ENC_HID_DIM, DEC_HID_DIM, ENC_DROPOUT)
+    dec = Decoder(OUTPUT_DIM, DEC_EMB_DIM, ENC_HID_DIM, DEC_HID_DIM, DEC_DROPOUT, attn)
+    model = Seq2Seq(enc, dec)
+    model.apply(init_weights)
+    return model
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, emb_dim, enc_hid_dim, dec_hid_dim, dropout):
+        super().__init__()
+        self.embedding = nn.Embedding(input_dim, emb_dim)
+        self.rnn = nn.GRU(emb_dim, enc_hid_dim, bidirectional=True)
+        self.fc = nn.Linear(enc_hid_dim * 2, dec_hid_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src):
+        embedded = self.dropout(self.embedding(src))
+        outputs, hidden = self.rnn(embedded)
+        hidden = torch.tanh(self.fc(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1)))
+        return outputs, hidden
+
+class Attention(nn.Module):
+    def __init__(self, enc_hid_dim, dec_hid_dim):
+        super().__init__()
+        self.attn = nn.Linear((enc_hid_dim * 2) + dec_hid_dim, dec_hid_dim)
+        self.v = nn.Linear(dec_hid_dim, 1, bias=False)
+
+    def forward(self, hidden, encoder_outputs):
+        batch_size = encoder_outputs.shape[1]
+        src_len = encoder_outputs.shape[0]
+        hidden = hidden.unsqueeze(1).repeat(1, src_len, 1)
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        energy = torch.tanh(self.attn(torch.cat((hidden, encoder_outputs), dim=2)))
+        attention = self.v(energy).squeeze(2)
+        return F.softmax(attention, dim=1)
+
+class Decoder(nn.Module):
+    def __init__(self, output_dim, emb_dim, enc_hid_dim, dec_hid_dim, dropout, attention):
+        super().__init__()
+        self.output_dim = output_dim
+        self.attention = attention
+        self.embedding = nn.Embedding(output_dim, emb_dim)
+        self.rnn = nn.GRU((enc_hid_dim * 2) + emb_dim, dec_hid_dim)
+        self.fc_out = nn.Linear((enc_hid_dim * 2) + dec_hid_dim + emb_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, input, hidden, encoder_outputs):
+        input = input.unsqueeze(0)
+        embedded = self.dropout(self.embedding(input))
+        a = self.attention(hidden, encoder_outputs)
+        a = a.unsqueeze(1)
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        weighted = torch.bmm(a, encoder_outputs)
+        weighted = weighted.permute(1, 0, 2)
+        rnn_input = torch.cat((embedded, weighted), dim=2)
+        output, hidden = self.rnn(rnn_input, hidden.unsqueeze(0))
+        assert (output == hidden).all()
+        embedded = embedded.squeeze(0)
+        output = output.squeeze(0)
+        weighted = weighted.squeeze(0)
+        prediction = self.fc_out(torch.cat((output, weighted, embedded), dim=1))
+        return prediction, hidden.squeeze(0)
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        dev = src.device
+        batch_size = src.shape[1]
+        trg_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(dev)
+        encoder_outputs, hidden = self.encoder(src)
+        input = trg[0, :]
+        for t in range(1, trg_len):
+            output, hidden = self.decoder(input, hidden, encoder_outputs)
+            outputs[t] = output
+            teacher_force = random.random() < teacher_forcing_ratio
+            top1 = output.argmax(1)
+            input = trg[t] if teacher_force else top1
+        return outputs
---
+++ flgo/benchmark/multi30k_translation/core.py
@@ -0,0 +1,172 @@
+import os
+import torch
+import torch.utils.data
+from flgo.benchmark.toolkits.nlp.classification import GeneralCalculator
+from flgo.benchmark.base import FromDatasetPipe, FromDatasetGenerator, BasicTaskCalculator
+from torchtext.data.functional import to_map_style_dataset
+try:
+    import ujson as json
+except:
+    import json
+from .config import train_data
+from .config import language_pair, tokenizers, vocabs
+from torch.nn.utils.rnn import pad_sequence
+try:
+    from .config import UNK_IDX
+except:
+    UNK_IDX = 0
+try:
+    from .config import PAD_IDX
+except:
+    PAD_IDX = 1
+try:
+    from .config import BOS_IDX
+except:
+    BOS_IDX = 2
+try:
+    from .config import EOS_IDX
+except:
+    EOS_IDX = 3
+try:
+    from .config import test_data
+except:
+    test_data = None
+try:
+    from .config import val_data
+except:
+    val_data = None
+
+def sequential_transforms(*transforms):
+    def func(txt_input):
+        for transform in transforms:
+            txt_input = transform(txt_input)
+        return txt_input
+    return func
+
+# function to add BOS/EOS and create tensor for input sequence indices
+def tensor_transform(token_ids):
+    return torch.cat((torch.tensor([BOS_IDX]),
+                      torch.tensor(token_ids),
+                      torch.tensor([EOS_IDX])))
+
+text_transform = {}
+for ln in language_pair:
+    text_transform[ln] = sequential_transforms(tokenizers[ln],  #Tokenization
+                                               vocabs[ln],  #Numericalization
+                                               tensor_transform) # Add BOS/EOS and create tensor
+
+def collate_fn(batch):
+    src_batch, tgt_batch = [], []
+    for src_sample, tgt_sample in batch:
+        src_batch.append(text_transform[language_pair[0]](src_sample.rstrip("\n")))
+        tgt_batch.append(text_transform[language_pair[1]](tgt_sample.rstrip("\n")))
+    src_batch = pad_sequence(src_batch, padding_value=PAD_IDX)
+    tgt_batch = pad_sequence(tgt_batch, padding_value=PAD_IDX)
+    return src_batch, tgt_batch
+
+class TaskGenerator(FromDatasetGenerator):
+    def __init__(self):
+        super(TaskGenerator, self).__init__(benchmark=os.path.split(os.path.dirname(__file__))[-1],
+                                            train_data=train_data, val_data=val_data, test_data=test_data)
+
+    def prepare_data_for_partition(self):
+        return to_map_style_dataset(self.train_data)
+
+class TaskPipe(FromDatasetPipe):
+    TaskDataset = torch.utils.data.Subset
+    def __init__(self, task_path):
+        super(TaskPipe, self).__init__(task_path, train_data=train_data, val_data=val_data, test_data=test_data)
+
+    def save_task(self, generator):
+        client_names = self.gen_client_names(len(generator.local_datas))
+        feddata = {'client_names': client_names,}
+        for cid in range(len(client_names)): feddata[client_names[cid]] = {'data': generator.local_datas[cid],}
+        with open(os.path.join(self.task_path, 'data.json'), 'w') as outf:
+            json.dump(feddata, outf)
+        return
+
+    def load_data(self, running_time_option) -> dict:
+        if self.test_data is not None:
+            test_data = to_map_style_dataset(self.test_data)
+            if self.val_data is not None:
+                server_test_data = test_data
+                server_val_data = to_map_style_dataset(self.val_data)
+            elif running_time_option['test_holdout'] > 0:
+                server_test_data, server_val_data = self.split_dataset(test_data, running_time_option['test_holdout'])
+            else:
+                server_test_data = test_data
+                server_val_data = None
+        elif self.val_data is not None:
+            server_test_data = None
+            server_val_data = to_map_style_dataset(self.val_data)
+        else:
+            server_test_data = server_val_data = None
+        # rearrange data for server
+        task_data = {'server': {'test': server_test_data, 'val': server_val_data}}
+        train_data = to_map_style_dataset(self.train_data)
+        for cid, cname in enumerate(self.feddata['client_names']):
+            cdata = self.TaskDataset(train_data, self.feddata[cname]['data'])
+            if running_time_option['train_holdout'] > 0:
+                cdata_train, cdata_valid = self.split_dataset(cdata, running_time_option['train_holdout'])
+                if running_time_option['local_test']:
+                    cdata_valid, cdata_test = self.split_dataset(cdata_valid, 0.5)
+                else:
+                    cdata_test = None
+            else:
+                cdata_train = cdata
+                cdata_valid, cdata_test = None, None
+            task_data[cname] = {'train': cdata_train, 'valid': cdata_valid, 'test': cdata_test}
+        return task_data
+
+class TaskCalculator(BasicTaskCalculator):
+    def __init__(self, device, optimizer_name='sgd'):
+        super(TaskCalculator, self).__init__(device, optimizer_name)
+        self.DataLoader = torch.utils.data.DataLoader
+        self.collect_fn = collate_fn
+        self.criterion = self.loss_func
+
+    def loss_func(self, outputs, targets, ignore_index=-100):
+        loss_func = torch.nn.CrossEntropyLoss(ignore_index=ignore_index)
+        return loss_func(outputs[1:].view(-1, outputs.shape[-1]), targets[1:].view(-1))
+
+    def compute_loss(self, model, data):
+        """
+        Args:
+            model: the model to train
+            data: the training dataset
+        Returns: dict of train-one-step's result, which should at least contains the key 'loss'
+        """
+        sources, targets = self.to_device(data)
+        outputs = model(sources, targets)
+        loss = self.criterion(outputs, targets, PAD_IDX)
+        return {'loss': loss}
+
+    @torch.no_grad()
+    def test(self, model, dataset, batch_size=64, num_workers=0, pin_memory=False):
+        """
+        Metric = [mean_accuracy, mean_loss]
+
+        Args:
+            model:
+            dataset:
+            batch_size:
+        Returns: [mean_accuracy, mean_loss]
+        """
+        model.eval()
+        if batch_size==-1:batch_size=len(dataset)
+        data_loader = self.get_dataloader(dataset, batch_size=batch_size, num_workers=num_workers, pin_memory=pin_memory)
+        total_loss = 0.0
+        for batch_id, batch_data in enumerate(data_loader):
+            batch_data = self.to_device(batch_data)
+            outputs = model(batch_data[0], batch_data[1])
+            batch_mean_loss = self.criterion(outputs, batch_data[1], model.ignore_index if hasattr(model, 'ignore_index') else -100).item()
+            total_loss += batch_mean_loss * batch_data[-1].shape[-1]
+        return {'loss':total_loss/len(dataset)}
+
+    def to_device(self, data):
+        return data[0].to(self.device), data[1].to(self.device)
+
+    def get_dataloader(self, dataset, batch_size=64, shuffle=True, num_workers=0, pin_memory=False, drop_last=False):
+        if self.DataLoader == None:
+            raise NotImplementedError("DataLoader Not Found.")
+        return self.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, pin_memory=pin_memory, drop_last=drop_last, collate_fn=self.collect_fn)
