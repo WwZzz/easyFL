45d960632692acbc8fb1c0495fb2880a40d09532
WwZzz <zwang@stu.xmu.edu.cn>
2023-09-03 23:32:59 +0800

    update apfl

--- resources/algorithm/apfl.py
+++ resources/algorithm/apfl.py
@@ -32,14 +32,18 @@ class Client(flgo.algorithm.fedbase.BasicClient):
             loss = loss_w + loss_lm
             loss.backward()
             if self.clip_grad>0:torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=self.clip_grad)
+            w_optimizer.step()
+            with torch.no_grad():
+                for pv, plm in zip(self.v.parameters(),self.model.parameters()):
+                    pv.data = pv.data - self.learning_rate*plm.grad.data
+            self.model = self.alpha*self.v + (1-self.alpha)*model
+            loss_new = self.calculator.compute_loss(self.model, batch_data)['loss']
+            loss_new.backward()
             with torch.no_grad():
                 dv = self.v - model
                 dalpha = 0.0
                 for pdv, plm in zip(dv.parameters(), self.model.parameters()):
-                    dalpha += (pdv*plm).sum()
+                    dalpha += (pdv*plm.grad).sum()
                 self.alpha = self.alpha - self.learning_rate * dalpha
-                for pv, plm in zip(self.v.parameters(),self.model.parameters()):
-                    pv.data = pv.data - self.learning_rate*plm.grad.data
-            w_optimizer.step()
-            self.model = self.alpha*self.v + (1-self.alpha)*model
+                self.alpha = max(min(self.alpha, 1.0), 0.0)
         return
