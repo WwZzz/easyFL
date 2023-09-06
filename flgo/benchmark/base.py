b22db82543533df7c343936b247b8626392eab27
zzz <zzz510711928@gmail.com>
2023-09-06 12:08:30 +0800

    forbidden to return empty dataset when spliting data

--- flgo/benchmark/base.py
+++ flgo/benchmark/base.py
@@ -304,7 +304,10 @@ class BasicTaskPipe(AbstractTaskPipe):
         if p == 0: return dataset, None
         s1 = int(len(dataset) * p)
         s2 = len(dataset) - s1
-        return torch.utils.data.random_split(dataset, [s2, s1])
+        if s1==0: return dataset, None
+        elif s2==0: return None, dataset
+        else:
+            return torch.utils.data.random_split(dataset, [s2, s1])
 
     def task_exists(self):
         r"""
