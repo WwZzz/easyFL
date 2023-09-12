d783e4b17cf40f974003aa2b64573b018ccf0def
WwZzz <zwang@stu.xmu.edu.cn>
2023-09-12 14:53:33 +0800

    add points segmentation

---
+++ flgo/benchmark/toolkits/cv/points_part_segmentation/temp/config.py
@@ -0,0 +1,14 @@
+"""
+train_data (torch.utils.data.Dataset),
+test_data (torch.utils.data.Dataset),
+and the model (torch.nn.Module) should be implemented here.
+
+"""
+import torch.nn
+
+train_data = None
+val_data = None
+test_data = None
+
+def get_model(*args, **kwargs) -> torch.nn.Module:
+    raise NotImplementedError
---
+++ flgo/benchmark/toolkits/cv/points_part_segmentation/temp/model/default_model.py
@@ -0,0 +1,19 @@
+from ..config import get_model
+from flgo.utils.fmodule import FModule
+
+class Model(FModule):
+    def __init__(self):
+        super().__init__()
+        self.model = get_model()
+        if hasattr(self.model, 'compute_loss'):
+            self.compute_loss = self.model.compute_loss
+
+    def forward(self, *args, **kwargs):
+        return self.model(*args, **kwargs)
+
+def init_local_module(object):
+    pass
+
+def init_global_module(object):
+    if 'Server' in object.__class__.__name__:
+        object.model = Model().to(object.device)
