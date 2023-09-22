125ebf44fbd4f074f9f078de336b241b70927ae8
WwZzz <zwang@stu.xmu.edu.cn>
2023-09-22 23:17:14 +0800

    add lines of info

--- flgo/algorithm/realbase.py
+++ flgo/algorithm/realbase.py
@@ -1,4 +1,5 @@
 import collections
+import os.path
 import pickle
 
 import torch.cuda
@@ -95,19 +96,29 @@ class Server(fedavg.Server):
             self._check_timestamp = time.time()
         return len(self.clients)>=10 or time.time()-self._check_timestamp>=30
 
+    def register_handler(self, worker_id, client_id, received_pkg):
+        valid_keys = ['num_steps', 'learning_rate', 'batch_size', 'momentum', 'weight_decay', 'num_epochs', 'optimizer']
+        if received_pkg["name"] not in self.clients.keys():
+            self.add_client(received_pkg["name"])
+            l = len(self.clients)
+            self.logger.info("%s joined in the federation. The number of clients is %i" % (received_pkg['name'], l))
+
+            d = {"client_idx": l, 'port_send': self.port_send, 'port_recv': self.port_recv,
+                 '__option__': {k: self.option[k] for k in valid_keys}}
+            self.registrar.send_multipart([worker_id, client_id, pickle.dumps(d, pickle.DEFAULT_PROTOCOL)])
+        else:
+            self.logger.info("%s rebuilt the connection." % received_pkg['name'])
+            self.registrar.send_pyobj({"client_idx": len(self.clients), 'port_send': self.port_send, 'port_recv': self.port_recv, '__option__': {k: self.option[k] for k in valid_keys}})
+
     def listen_for_registration(self):
         while not self.is_exit():
             time.sleep(0.1)
             if len(self.register_poller.poll(10000)) > 0:
-                reg_req = self.registrar.recv_pyobj()
-                if reg_req["name"] not in self.clients.keys():
-                    self.add_client(reg_req["name"])
-                    l = len(self.clients)
-                    self.logger.info("%s joined in the federation. The number of clients is %i" % (reg_req['name'], l))
-                    valid_keys = ['num_steps', 'learning_rate', 'batch_size', 'momentum', 'weight_decay', 'num_epochs', 'optimizer']
-                    self.registrar.send_pyobj({"client_idx": l, 'port_send': self.port_send, 'port_recv': self.port_recv, '__option__':{k:self.option[k] for k in valid_keys}})
-                else:
-                    self.logger.info("%s rebuilt the connection." % reg_req['name'])
+                worker_id = self.registrar.recv()
+                client_id = self.registrar.recv()
+                received_pkg = self.registrar.recv_pyobj()
+                t = threading.Thread(self.register_handler, args=(worker_id, client_id, received_pkg))
+                t.start()
 
     def listen_for_sender(self):
         # t = threading.current_thread()
@@ -124,6 +135,29 @@ class Server(fedavg.Server):
                 else:
                     self.add_buffer({'name':name, 'package':d})
                     self.logger.info("Received package of size {}MB from {} at round {}".format(package_size, name, self.current_round))
+
+    # def listen_for_pulling_task(self):
+    #     while not self.is_exit():
+    #         time.sleep(0.1)
+    #         if len(self.register_poller.poll(10000)) > 0:
+    #             reg_req = self.registrar.recv_pyobj()
+    #             if '__mtype__' in reg_req and reg_req=='pulling':
+    #                 try:
+    #                     self.logger.info("Push public task...")
+    #                     # self.registrar.send()
+    #                 except Exception as e:
+    #                     self.logger.info(str(e))
+    #                     self.logger.info("Failed to push task.")
+    #                 continue
+    #             else:
+    #                 if reg_req["name"] not in self.clients.keys():
+    #                     self.add_client(reg_req["name"])
+    #                     l = len(self.clients)
+    #                     self.logger.info("%s joined in the federation. The number of clients is %i" % (reg_req['name'], l))
+    #                     valid_keys = ['num_steps', 'learning_rate', 'batch_size', 'momentum', 'weight_decay', 'num_epochs', 'optimizer']
+    #                     self.registrar.send_pyobj({"client_idx": l, 'port_send': self.port_send, 'port_recv': self.port_recv, '__option__':{k:self.option[k] for k in valid_keys}})
+    #                 else:
+    #                     self.logger.info("%s rebuilt the connection." % reg_req['name'])
 
     @property
     def clients(self):
@@ -152,16 +186,20 @@ class Server(fedavg.Server):
     def run(self, ip='*', port='5555'):
         self.logger = self.logger(task=self.option['task'], option=self.option, name=self.name+'_'+str(self.logger), level=self.option['log_level'])
         self.logger.register_variable(object=self, server=self)
+        task_name = os.path.basename(self.option['task'])
+        if os.path.exists(os.path.join(os.path.dirname(self.option['task']), task_name+'.zip')):
+            flgo.zip_task(self.option['task'], target_path='.')
         self._clients = {}
+        self.ip = ip
+        self.port = port
         self._lock_registration = threading.Lock()
         self._buffer = mlp.Queue()
         self._lock_buffer = threading.Lock()
         self._exit = False
         self._lock_exit = threading.Lock()
-        self.ip = ip
-        self.port = port
+
         self.context = zmq.Context()
-        self.registrar = self.context.socket(zmq.REP)
+        self.registrar = self.context.socket(zmq.ROUTER)
         self.registrar.bind("tcp://%s:%s" % (ip, port))
         self.port_send = self.get_free_port()
         self.sender = self.context.socket(zmq.PUB)
