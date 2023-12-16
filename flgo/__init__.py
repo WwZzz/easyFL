b52352da0e382675e86e95d99e54374506b484fb
zzz <zzz510711928@gmail.com>
2023-12-16 15:36:15 +0800

    increase the version

--- flgo/__init__.py
+++ flgo/__init__.py
@@ -3,7 +3,7 @@ from .benchmark import data_root
 communicator = None
 _data_root = data_root
 _name = None
-__version__ = "v1.0.10"
+__version__ = "v1.0.11"
 class VirtualCommunicator:
     """
     Communicator that simulates the communication phase between any two objects
