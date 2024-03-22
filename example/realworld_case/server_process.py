44abe7ecccf57b1785e68049c79cb6cc1c71b83b
zzz <zzz510711928@gmail.com>
2024-03-22 16:46:13 +0800

    add examples

--- example/realworld_case/server_process.py
+++ example/realworld_case/server_process.py
@@ -8,10 +8,10 @@ task = './real_synthetic'
 flgo.gen_real_task(synthetic, 'synthetic_dataset.py', task)
 server_runner = flgo.init(task, realbase, {'save_checkpoint':'1', 'proportion':1.0, 'num_rounds':30, 'learning_rate':0.1, 'num_epochs':5}, scene='real_hserver')
 
-# clients who lose connections for >=availability_timeout seconds will be viewed unavailable
+# clients who lose connections for >=availability_timeout seconds will be viewed unavailable. Default value is 30
 server_runner.set_availability_timeout(30)
-# clients who doesn't send back model till communication_timeout seconds will be viewed delayed
+# clients who doesn't send back model till communication_timeout seconds will be viewed delayed. Default value is 30
 server_runner.set_communication_timeout(10)
-# wait for training start until num_clients>=2
-server_runner.register_start_condition(lambda server: server.num_clients>=3)
+# wait for training start until num_clients>=2. Default condition is the same
+server_runner.register_start_condition(lambda server: server.num_clients>=2)
 server_runner.run(ip=server_ip, port="5555", port_task='7777')
