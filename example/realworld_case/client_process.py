f3ab01adcdbd86c906a542a6787b4ad125646a6a
zzz <zzz510711928@gmail.com>
2024-03-22 14:47:25 +0800

    add examples

--- example/realworld_case/client_process.py
+++ example/realworld_case/client_process.py
@@ -1,7 +1,8 @@
 import flgo
 import flgo.algorithm.realbase as realbase
 
+server_ip = None
+
 task = 'real_synthetic'
-target_path = 'client_1'
-flgo.pull_task_from_('tcp://127.0.0.1:7777', task, target_path=target_path)
-flgo.init(task, realbase, scene='real_hclient').run(server_ip='127.0.0.1', server_port="5555")
+flgo.pull_task_from_('tcp://%s:7777'%server_ip, task)
+flgo.init(task, realbase, scene='real_hclient').run(server_ip=server_ip, server_port="5555")
--- example/realworld_case/server_process.py
+++ example/realworld_case/server_process.py
@@ -2,6 +2,8 @@ import flgo
 import flgo.benchmark.synthetic_regression as synthetic
 import flgo.algorithm.realbase as realbase
 
+server_ip = None
+
 task = './real_synthetic'
 flgo.gen_real_task(synthetic, 'synthetic_dataset.py', task)
-flgo.init(task, realbase, {'save_checkpoint':'1', 'proportion':1.0},scene='real_hserver').run(ip='127.0.0.1', port="5555", port_task='7777')
+flgo.init(task, realbase, {'save_checkpoint':'1', 'proportion':1.0}, scene='real_hserver').run(ip=server_ip, port="5555", port_task='7777')
