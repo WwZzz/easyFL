17005d5895477a53dfe047b7ffa748d9ac83c832
zzz <zzz510711928@gmail.com>
2024-03-22 15:12:54 +0800

    add examples

--- example/realworld_case/server_process.py
+++ example/realworld_case/server_process.py
@@ -6,5 +6,5 @@ server_ip = '127.0.0.1'
 
 task = './real_synthetic'
 flgo.gen_real_task(synthetic, 'synthetic_dataset.py', task)
-server_runner = flgo.init(task, realbase, {'save_checkpoint':'1', 'proportion':1.0, 'num_rounds':20, 'learning_rate':0.1}, scene='real_hserver')
+server_runner = flgo.init(task, realbase, {'save_checkpoint':'1', 'proportion':1.0, 'num_rounds':20, 'learning_rate':0.1, 'num_epochs':5}, scene='real_hserver')
 server_runner.run(ip=server_ip, port="5555", port_task='7777')
