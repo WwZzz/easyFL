919a6c1a2723189fdf9c7f0bf87fc745f06842d8
zzz <zzz510711928@gmail.com>
2024-03-22 14:18:17 +0800

    fix bugs of pull task

--- flgo/utils/fflow.py
+++ flgo/utils/fflow.py
@@ -1386,7 +1386,8 @@ def pull_task_from_(address:str, task_name:str, target_path='.', unzip=True, tim
     task_zip = os.path.basename(task_name)+'.zip'
     zip_path = os.path.join(target_path, task_zip)
     if os.path.exists(zip_path):
-        raise FileExistsError("Zipped task {} already exists.".format(zip_path))
+        warnings.warn("Zipped task {} already exists. Please Remove this task before the next pulling.".format(zip_path))
+        return
     # ctx = zmq.Context()
     sck = _ctx.socket(zmq.REQ)
     sck.setsockopt(zmq.RCVTIMEO, timeout*1000)
